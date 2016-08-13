9a9a4070c50316cdaae78dc5b6d36d2435c97a73
zhleternity <1021062123@qq.com>
2016-08-13 15:53:55 +0800

    def2

--- features.py
+++ features.py
@@ -9,7 +9,23 @@ def extract_features(imgs, feature_fns, verbose=False):
     and save as a feature array
     Inputs:
     :param imgs: N x H X W x C array of pixel data for N images
-    :param feature_fns: List of k feature function
-    :param verbose:
+    :param feature_fns: List of k feature functions,the ith feature function should take
+    as input an H x W x D array and return a (one-dimensional)array of length F_i.
+    :param verbose:if true,print progress
     :return:
+    an array of shape(F_1 + ... + F_k, N) where each column is the concatenation of all features for a single image.
     """
+
+    num_images = imgs.shape[0]
+    if num_images == 0:
+        return np.array([])
+    #use the first image to determine feature dimensions
+    feature_dims = []
+    first_image_features = []
+    for feature_fn in feature_fns:
+        feats = feature_fn(imgs[0].squeeze())
+        assert len(feats.shape) == 1, 'Feature functions must be one-dimensional'
+        feature_dims.append(feats.size)
+        first_image_features.append(feats)
+    #now that we know the dimensions of the features, we can allocate a single
+    #big array to store all features as columns
