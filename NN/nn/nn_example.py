dd2ab99ccb5969cd72585d43093d025b51ff1990
zhleternity <1021062123@qq.com>
2016-08-25 12:10:18 +0800

    LR

--- NN/nn/nn_example.py
+++ NN/nn/nn_example.py
@@ -55,11 +55,34 @@ if __name__ == '__main__':
                     z1 = x.dot(W1) + b1
                     a1 = np.tanh(z1)
                     z2 = a1.dot(W2) + b2
-                    exp_scores = np.exp(z2)
+                    exp_scores = np.exp(z2)  #  softmax clssifier
                     probs = exp_scores / np.sum(exp_scores, axis=1, keepdims=True)
 
                     # compute loss
+                    correct_logprobs = -np.log(probs[range(num_examples), y])
+                    data_loss = np.sum(correct_logprobs)
+                    data_loss += reg/2 * (np.sum(np.square(W1)) + np.sum(np.square(W2)))
+                    return 1.0 / num_examples * data_loss
+
+
                     
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
