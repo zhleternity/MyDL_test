3e6c8dcaadc68b89fc2b587960c1d6c746285df4
zhleternity <1021062123@qq.com>
2016-08-25 12:00:52 +0800

    LR

--- NN/nn/nn_example.py
+++ NN/nn/nn_example.py
@@ -31,7 +31,7 @@ if __name__ == '__main__':
                 z = pred_func(np.c_[xx.ravel(), yy.ravel()])  #  ravel():Return a contiguous flattened array
                 #  np.c_: concatenate alone the second axis
 
-                z1 = z.reshape(xx.shape)
+                z = z.reshape(xx.shape)
 
                 #  use LR to classify
                 #  Firstly,see the LR effect
@@ -39,7 +39,31 @@ if __name__ == '__main__':
                 clf.fit(x, y)
 
                 #  Obviously,the classification result is not satisfied,so we try to use NN to do it
-                
+                num_examples = len(x)  #  number of samples
+                nn_input_dim = 2  #  dims of input
+                nn_output_dim = 2  #  number of output classes
+
+                #GD params
+                lr = 0.01  #  learning rate
+                reg = 0.01  #  regularization
+
+                #  define loss func
+                def calculate_loss(model):
+                    W1, b1, W2, b2 = model['W1'], model['b1'], model['W2'], model['b2']
+
+                    # forward computing
+                    z1 = x.dot(W1) + b1
+                    a1 = np.tanh(z1)
+                    z2 = a1.dot(W2) + b2
+                    exp_scores = np.exp(z2)
+                    probs = exp_scores / np.sum(exp_scores, axis=1, keepdims=True)
+
+                    # compute loss
+                    
+
+
+
+
 
 
 
